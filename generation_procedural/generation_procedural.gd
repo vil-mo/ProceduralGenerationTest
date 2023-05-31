54c3ec3a97b8c15d359120348ac72c8e41d9bec1
vil-mo <morgraphvillain@gmail.com>
2023-05-31 19:30:36 +0500

    Cleaned code, added terrains

--- generation_procedural/generation_procedural.gd
+++ generation_procedural/generation_procedural.gd
@@ -1,12 +1,19 @@
 extends TileMap
 
-# layer 0 is for tiles
+const BASE_LAYER = 0
+const GROUND_LAYER = 1
+const WALL_LAYER = 2
+
+const BASE_ATLAS = 0
+const USED_TERRAIN_SET = 0
+const GROUND_TERRAIN = 0
+const WALL_TERRAIN = 1
 
 var size := Vector2i(20, 20)
 
 var holes_amount : int = 3
 var initial_ground_removing_amount : int = 4
-var initial_ground_chance_to_be_removed : float = 0.25
+var initial_ground_chance_to_be_removed : float = 0.17
 var walls_amount : int = 3
 var walls_neghbor_amount : int = 4
 var walls_expanding_amount : int = 1
@@ -20,7 +27,7 @@ const tiles := {
 	wall = Vector2i(2, 0),
 }
 
-const sides_and_corners : Array[int] = [
+const SIDES_AND_CORNERS : Array[int] = [
 	TileSet.CELL_NEIGHBOR_RIGHT_SIDE, 
 	TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER,
 	TileSet.CELL_NEIGHBOR_BOTTOM_SIDE, 
@@ -30,31 +37,35 @@ const sides_and_corners : Array[int] = [
 	TileSet.CELL_NEIGHBOR_TOP_SIDE, 
 	TileSet.CELL_NEIGHBOR_TOP_RIGHT_CORNER,
 ]
-const sides : Array[int] = [
+const SIDES : Array[int] = [
 	TileSet.CELL_NEIGHBOR_RIGHT_SIDE, 
 	TileSet.CELL_NEIGHBOR_BOTTOM_SIDE, 
 	TileSet.CELL_NEIGHBOR_LEFT_SIDE, 
 	TileSet.CELL_NEIGHBOR_TOP_SIDE, 
 ]
 
-var waiting_time_for_demonstration = .5
+var waiting_time_for_demonstration = 0.5
+
+
 
 func generate_room(should_connect_land : bool = true):
+	
+	
 	# Filling all up ========================================================
 	for x in range(-5, size.x + 5):
 		for y in range(-5, size.y + 5):
-			set_cell(0, Vector2i(x, y), 0, tiles.empty)
+			set_cell(BASE_LAYER, Vector2i(x, y), BASE_ATLAS, tiles.empty)
 	
 	for x in size.x:
 		for y in size.y:
-			set_cell(0, Vector2i(x, y), 0, tiles.ground)
+			set_cell(BASE_LAYER, Vector2i(x, y), BASE_ATLAS, tiles.ground)
 	
 	await get_tree().create_timer(waiting_time_for_demonstration).timeout
 	# Making holes ==================================================
 	
-	var ground_cells := get_used_cells_by_id(0, -1, tiles.ground)
+	var ground_cells := get_used_cells_by_id(BASE_LAYER, -1, tiles.ground)
 	for i in holes_amount:
-		set_cell(0, ground_cells.pick_random(), 0, tiles.empty)
+		set_cell(BASE_LAYER, ground_cells.pick_random(), BASE_ATLAS, tiles.empty)
 	
 	#Removing tiles at edges randomly
 	
@@ -62,28 +73,29 @@ func generate_room(should_connect_land : bool = true):
 		
 		await get_tree().create_timer(waiting_time_for_demonstration).timeout
 		
-		ground_cells = get_used_cells_by_id(0, -1, tiles.ground)
+		ground_cells = get_used_cells_by_id(BASE_LAYER, -1, tiles.ground)
 		
 		var to_remove_coords : Array[Vector2i] = []
 		
 		for cell in ground_cells:
-			var sides_maching : Array[Vector2i] = get_neighbors_of_atlas(0, cell, [tiles.empty], sides_and_corners)
+			var sides_maching : Array[Vector2i] = get_neighbors_of_atlas(BASE_LAYER, cell, [tiles.empty], SIDES_AND_CORNERS)
 			if randf() < sides_maching.size() * initial_ground_chance_to_be_removed:
 				to_remove_coords.append(cell)
 		
 		for coord in to_remove_coords:
-			set_cell(0, coord, 0, tiles.empty)
+			set_cell(BASE_LAYER, coord, BASE_ATLAS, tiles.empty)
 	
 	
 	await get_tree().create_timer(waiting_time_for_demonstration).timeout
-	# Smoothing edges ======================================
+	# Smoothing ======================================
 	const what_tile_becomes_if_too_many_sides := {
 		tiles.empty : tiles.ground,
 		tiles.ground : tiles.empty,
+		tiles.wall : tiles.wall
 	}
 	
-	var used_cells := get_used_cells(0)
-	var last_atlas := get_atlas_of_cells(0, used_cells)
+	var used_cells := get_used_cells(BASE_LAYER)
+	var last_atlas := get_atlas_of_cells(BASE_LAYER, used_cells)
 	
 	for i in 10:
 		var to_remove_coords : Array[Vector2i] = []
@@ -91,76 +103,76 @@ func generate_room(should_connect_land : bool = true):
 		for x in size.x:
 			for y in size.y:
 				var cell := Vector2i(x, y)
-				var atlas := get_cell_atlas_coords(0, cell)
-				var sides_maching : Array[Vector2i] = get_neighbors_of_atlas(0, cell, [atlas], sides)
+				var atlas := get_cell_atlas_coords(BASE_LAYER, cell)
+				var sides_maching : Array[Vector2i] = get_neighbors_of_atlas(BASE_LAYER, cell, [atlas], SIDES)
 				
 				if sides_maching.size() <= 1:
 					to_remove_coords.append(cell)
 		
 		for coord in to_remove_coords:
-			set_cell(0, coord, 0, what_tile_becomes_if_too_many_sides[get_cell_atlas_coords(0, coord)])
+			set_cell(BASE_LAYER, coord, BASE_ATLAS, what_tile_becomes_if_too_many_sides[get_cell_atlas_coords(BASE_LAYER, coord)])
 		
-		var current_atlas := get_atlas_of_cells(0, used_cells)
+		var current_atlas := get_atlas_of_cells(BASE_LAYER, used_cells)
 		if current_atlas == last_atlas:
 			break
 		last_atlas = current_atlas
 	
 	await get_tree().create_timer(waiting_time_for_demonstration).timeout
 	# Walls instantiating ====================================
-	ground_cells = get_used_cells_by_id(0, -1, tiles.ground)
+	ground_cells = get_used_cells_by_id(BASE_LAYER, -1, tiles.ground)
 
 	for i in walls_amount:
 		var current_wall = ground_cells.pick_random()
-		set_cell(0, current_wall, 0, tiles.wall)
+		set_cell(BASE_LAYER, current_wall, BASE_ATLAS, tiles.wall)
 		for j in walls_neghbor_amount - 1:
-			var random_neighbor := get_neighbor_cell(current_wall, sides_and_corners.pick_random())
-			if tiles.ground == get_cell_atlas_coords(0, random_neighbor):
+			var random_neighbor := get_neighbor_cell(current_wall, SIDES_AND_CORNERS.pick_random())
+			if tiles.ground == get_cell_atlas_coords(BASE_LAYER, random_neighbor):
 				current_wall = random_neighbor
-				set_cell(0, current_wall, 0, tiles.wall)
+				set_cell(BASE_LAYER, current_wall, BASE_ATLAS, tiles.wall)
 	
 	# Expanding walls ====================================
 	for i in walls_expanding_amount:
 		
 		await get_tree().create_timer(waiting_time_for_demonstration).timeout
 		
-		ground_cells = get_used_cells_by_id(0, -1, tiles.ground)
+		ground_cells = get_used_cells_by_id(BASE_LAYER, -1, tiles.ground)
 		
 		var to_expand_coords : Array[Vector2i] = []
 		
 		for cell in ground_cells:
-			var wall_neighbors := get_neighbors_of_atlas(0, cell, [tiles.wall], sides_and_corners)
+			var wall_neighbors := get_neighbors_of_atlas(BASE_LAYER, cell, [tiles.wall], SIDES_AND_CORNERS)
 			
 			if randf() < wall_neighbors.size() * walls_chance_to_be_expanded:
 				to_expand_coords.append(cell)
 		for coord in to_expand_coords:
-			set_cell(0, coord, 0, tiles.wall)
+			set_cell(BASE_LAYER, coord, BASE_ATLAS, tiles.wall)
 	
 	await get_tree().create_timer(waiting_time_for_demonstration).timeout
 	# Smoothing walls ====================================
-	ground_cells = get_used_cells_by_id(0, -1, tiles.ground)
+	ground_cells = get_used_cells_by_id(BASE_LAYER, -1, tiles.ground)
 	
 	for cell in ground_cells:
-		var wall_neighbors : Array[Vector2i] =  get_neighbors_of_atlas(0, cell, [tiles.wall], sides)
+		var wall_neighbors : Array[Vector2i] =  get_neighbors_of_atlas(BASE_LAYER, cell, [tiles.wall], SIDES)
 		
 		if wall_neighbors.size() >= 2:
-			set_cell(0, cell, 0, tiles.wall)
+			set_cell(BASE_LAYER, cell, BASE_ATLAS, tiles.wall)
 	
 	await get_tree().create_timer(waiting_time_for_demonstration).timeout
 	# Filling up ground so that walls not near the empty ======================================
 	
-	used_cells = get_used_cells(0)
-	last_atlas = get_atlas_of_cells(0, used_cells)
+	used_cells = get_used_cells(BASE_LAYER)
+	last_atlas = get_atlas_of_cells(BASE_LAYER, used_cells)
 	
 	while true:
-		ground_cells = get_used_cells_by_id(0, -1, tiles.ground)
+		ground_cells = get_used_cells_by_id(BASE_LAYER, -1, tiles.ground)
 		
 		for cell in ground_cells:
-			var wall_neighbors = get_neighbors_of_atlas(0, cell, [tiles.wall], sides_and_corners)
+			var wall_neighbors = get_neighbors_of_atlas(BASE_LAYER, cell, [tiles.wall], SIDES_AND_CORNERS)
 			if wall_neighbors.size() > 0:
-				for empty in get_neighbors_of_atlas(0, cell, [tiles.empty], sides_and_corners):
-					set_cell(0, empty, 0, tiles.ground)
+				for empty in get_neighbors_of_atlas(BASE_LAYER, cell, [tiles.empty], SIDES_AND_CORNERS):
+					set_cell(BASE_LAYER, empty, BASE_ATLAS, tiles.ground)
 		
-		var current_atlas := get_atlas_of_cells(0, used_cells)
+		var current_atlas := get_atlas_of_cells(BASE_LAYER, used_cells)
 		if current_atlas == last_atlas:
 			break
 		last_atlas = current_atlas
@@ -169,7 +181,7 @@ func generate_room(should_connect_land : bool = true):
 	# Connecting land =======================================================
 	
 	if should_connect_land:
-		var land : Array[Array] = get_land(0, [tiles.ground])
+		var land : Array[Array] = get_land(BASE_LAYER, [tiles.ground])
 		
 		land.shuffle()
 		
@@ -178,20 +190,55 @@ func generate_room(should_connect_land : bool = true):
 			var land2 = land[i + 1]
 			var closest := find_closest_point_between_two_arrays(land1, land2)
 			
-			for cell in get_used_cells_by_id(0, -1, tiles.empty):
+			for cell in get_used_cells_by_id(BASE_LAYER, -1, tiles.empty):
 				var point := Geometry2D.get_closest_point_to_segment(cell, closest[0], closest[1])
 				if point.distance_squared_to(cell) <= connecting_land_connection_wideness:
-					set_cell(0, cell, 0, tiles.ground)
+					set_cell(BASE_LAYER, cell, BASE_ATLAS, tiles.ground)
 	
 	# Removing wall land =======================================================
 	
-	var wall_land := get_land(0, [tiles.wall])
-	var wall_and_ground_land := get_land(0, [tiles.wall, tiles.ground])
+	var wall_land := get_land(BASE_LAYER, [tiles.wall])
+	var wall_and_ground_land := get_land(BASE_LAYER, [tiles.wall, tiles.ground])
 	for l in wall_and_ground_land:
 		for w in wall_land:
 			if l == w:
 				for cell in l:
-					set_cell(0, cell, 0, tiles.empty)
+					set_cell(BASE_LAYER, cell, BASE_ATLAS, tiles.empty)
+	
+	await get_tree().create_timer(waiting_time_for_demonstration).timeout
+	# Smoothing up again ==================================================
+	used_cells = get_used_cells(BASE_LAYER)
+	last_atlas = get_atlas_of_cells(BASE_LAYER, used_cells)
+	
+	for i in 10:
+		var to_remove_coords : Array[Vector2i] = []
+		
+		for x in size.x:
+			for y in size.y:
+				var cell := Vector2i(x, y)
+				var atlas := get_cell_atlas_coords(BASE_LAYER, cell)
+				var sides_maching : Array[Vector2i] = get_neighbors_of_atlas(BASE_LAYER, cell, [atlas], SIDES)
+				
+				if sides_maching.size() <= 1:
+					to_remove_coords.append(cell)
+		
+		for coord in to_remove_coords:
+			set_cell(BASE_LAYER, coord, BASE_ATLAS, what_tile_becomes_if_too_many_sides[get_cell_atlas_coords(BASE_LAYER, coord)])
+		
+		var current_atlas := get_atlas_of_cells(BASE_LAYER, used_cells)
+		if current_atlas == last_atlas:
+			break
+		last_atlas = current_atlas
+	
+	
+	# Making terrains ===========================================================
+	
+	ground_cells = get_used_cells_by_id(BASE_LAYER, -1, tiles.ground)
+	var wall_cells = get_used_cells_by_id(BASE_LAYER, -1, tiles.wall)
+	ground_cells.append_array(wall_cells)
+	set_cells_terrain_connect(GROUND_LAYER, ground_cells, USED_TERRAIN_SET, GROUND_TERRAIN)
+	set_cells_terrain_connect(WALL_LAYER, wall_cells, USED_TERRAIN_SET, WALL_TERRAIN)
+	set_layer_enabled(BASE_LAYER, false)
 
 func get_land(layer : int, land_consists_of : Array[Vector2i]) -> Array[Array]:
 	var cells : Array[Vector2i] = []
@@ -212,7 +259,7 @@ func get_land(layer : int, land_consists_of : Array[Vector2i]) -> Array[Array]:
 func finding_land_recurtion(cell : Vector2i, land : Array[Array], atlas : Array[Vector2i]):
 	if !cell in land[-1]:
 		land[-1].append(cell)
-		var neighbors = get_neighbors_of_atlas(0, cell, atlas, sides)
+		var neighbors = get_neighbors_of_atlas(BASE_LAYER, cell, atlas, SIDES)
 		for neighbor in neighbors:
 			finding_land_recurtion(neighbor, land, atlas)
 
